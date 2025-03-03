cd9a92633fb0047ea2dc1eff16bbcbf21ae0dbe8
Sheffy <ishevchenko.id2000@yandex.ru>
2025-03-03 12:00:44 +0300

    Done

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,15 +1,24 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HelloController {
+
+	private final CarService carService;
+
+	public HelloController(CarService carService) {
+		this.carService = carService;
+	}
 
 	@GetMapping(value = "/")
 	public String printWelcome(ModelMap model) {
@@ -19,6 +28,12 @@ public class HelloController {
 		messages.add("5.2.0 version by sep'19 ");
 		model.addAttribute("messages", messages);
 		return "index";
+	}
+
+	@GetMapping(value = "/cars")
+	public String showCars(@RequestParam(value = "count", required = false, defaultValue = "5") int count, Model model){
+		model.addAttribute("cars", carService.getCars (count));
+		return "cars";
 	}
 	
 }
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,25 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+    private final List <Car> cars = new ArrayList <> ();
+    public CarServiceImpl() {
+        cars.add(new Car("Kia", "black", 2022));
+        cars.add(new Car("Hyundai", "white", 2017));
+        cars.add(new Car("BMW", "Gray", 2015));
+        cars.add(new Car("Mercedes", "black", 2023));
+        cars.add(new Car("Audi", "yellow", 2024));
+    }
+
+
+    @Override
+    public List<Car> getCars(int count) {
+        return cars.subList(0, count);
+    }
+}
